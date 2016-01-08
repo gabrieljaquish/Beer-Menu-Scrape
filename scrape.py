5d1f70243456a71c1fdf7b36490925e1532c6021
Gabriel Jaquish <gabriel.jaquish@northernharvestbrewing.com>
2016-01-08 15:39:11 -0500

    Scrape update, still in progress

--- scrape.py
+++ scrape.py
@@ -7,10 +7,14 @@ req = urllib2.Request('http://www.voidspace.org.uk')
 response = urllib2.urlopen(req)
 the_page = response.read()
 
+for line in the_page:
+    if line.find('table.beermenu')
+        the_page = line
+
+print(the_page)
 
 filename = time.strftime("%d-%m-%Y")
 
 with open(filename, 'w', newline='') as fp:
     csv_file = csv.writer(fp, delimiter=',')
     csv_file.writerows(data)
-
