96d6e917123745b28525113b05fea2e95cf6d5f0
Gabriel Jaquish <gabriel.jaquish@northernharvestbrewing.com>
2016-01-08 17:01:01 -0500

    Additional Progress, estracted table and pretified HTML

--- scrape.py
+++ scrape.py
@@ -3,18 +3,65 @@ import urllib2
 import time
 import csv
 
-req = urllib2.Request('http://www.voidspace.org.uk')
-response = urllib2.urlopen(req)
-the_page = response.read()
+def GetHTML(url):
+    #Pull down the menu number's widget page
+    req = urllib2.Request(url)
+    response = urllib2.urlopen(req)
+    #Use .readLINES() here to get html line by line
+    return response.readlines()
 
-for line in the_page:
-    if line.find('table.beermenu')
-        the_page = line
+def ScrapeMenu(menu_num):
+    #Beer Menus widget base url
+    url_base = 'https://www.beermenus.com/menu_widgets/'
 
-print(the_page)
+    #Beer Menus baseurl + the menue number we want
+    url = url_base + menu_num
 
-filename = time.strftime("%d-%m-%Y")
+    #Get the HTML from the page using the get HTML function
+    raw_page = GetHTML(url)
 
-with open(filename, 'w', newline='') as fp:
-    csv_file = csv.writer(fp, delimiter=',')
-    csv_file.writerows(data)
+    #Array of strings with \ stripped out
+    the_page = []
+
+    #Remove the backslashes from each line as they mess up BeautifulSoup
+    for line in raw_page:
+        if line.find('table') != -1:
+            #REMOVE Escape Chars
+            #remove \n's
+            line = line.replace("\\n","")
+            #make \" just "
+            line = line.replace("\\\"","\"")
+            #make \/ just /
+            line = line.replace("\\/","/")
+            the_page.append(line)
+            soup = BeautifulSoup(the_page[0], 'html.parser')
+            print(soup.prettify())
+
+
+#open the file containing the menu numbers to scrape
+with open("menu-numbers.txt") as f:
+    MenuNumbersFile = f.readlines()
+
+menu_numbers = []
+#loop through the file to get menu numbers
+#lines that start with # are comments ignored, just like in python
+
+
+for line in MenuNumbersFile:
+    if line.startswith("#"):
+        print(line)
+    else:
+        #NEEDS ERROR CHECKING
+        menu_numbers.append(line)
+
+for menu in menu_numbers:
+    ScrapeMenu(menu)
+
+
+#Name the scrape file with todays date + the menu number
+date_filename = time.strftime("%d-%m-%Y") + "_" + "1234"
+
+#testing
+print(date_filename)
+
+#Write the file as a csv to disk using the date-filename
